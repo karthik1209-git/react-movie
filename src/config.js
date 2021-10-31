ef52b4a4f6c739f0d1a4b555f0355b86b936a08c
Karthik Chaithanya Papatla <72907905+karthik1209-git@users.noreply.github.com>
2021-10-31 12:16:41 +0530

    API Reconfigured

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // Configuration for TMDB
 // To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
-//https://api.themoviedb.org/3/movie/550?api_key=2c57c268af6a288b72115032a2148394
+
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '2c57c268af6a288b72115032a2148394';
+const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
 
 // Images
 // An image URL looks like this example:
